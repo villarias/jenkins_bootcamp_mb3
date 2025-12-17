b25cb864d692c0900626aa6ea27ee408d0d53457
Villarias <pablovillarias1998@gmail.com>
2025-12-17 12:14:54 +0100

    eliminar fichero subido

--- main.py
+++ main.py
@@ -3,14 +3,7 @@ from pathlib import Path
 print('Ejecutando el script de python')
 nombre = sys.argv[1]
 edad = sys.argv[2]
-# file_path = Path(sys.argv[3])
 
-# if not file_path.exists():
-#     raise FileNotFoundError(f"No existe el archivo: {file_path}")
-
-# with file_path.open() as f:
-#     for line in f:
-#         print(line.strip())
         
 print('Mostrando variables de parametros')
 print(f"El nombre pasado por parámetros es : {nombre}")
